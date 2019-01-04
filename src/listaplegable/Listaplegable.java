668135ce203f34181053ee8832093fbf9d1f4572
maalc <maalc@192.168.1.117>
2019-01-04 13:08:11 -0300

    Mis practicas subidas a Github

---
+++ src/listaplegable/Listaplegable.java
@@ -0,0 +1,16 @@
+
+package listaplegable;
+
+
+public class Listaplegable {
+
+ 
+    public static void main(String[] args) {
+        
+        pantalla pp = new pantalla();
+        pp.setVisible(true);
+        pp.setLocationRelativeTo(null);
+        pp.setTitle("Lista Desplegable con Array");
+    }
+    
+}
